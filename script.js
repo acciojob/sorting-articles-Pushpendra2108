aad7b91deb8749f5046d94fbe235bc4f6d1ff0c6
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 13:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+ function() {
+  const list = document.getElementById("band-list"); 
+  const items = [...list.children];
+
+  items.sort((a, b) => a.textContent.localeCompare(b.textContent)); 
+
+  // Append sorted items back to the <ul>
+  items.forEach(item => list.appendChild(item));
+};
